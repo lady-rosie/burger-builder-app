40cca168c4a13d4638bd928e9e95b2463bf898a9
Rose Grondin <rose.grondin@gmail.com>
2020-10-13 15:27:19 -0400

    create the burger and display it on the screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurderBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout>
+          <BurderBuilder/> 
+        </Layout>
       </div>
     );
   }
