849febefcec68fc1001043d1ad6b502707ceab03
Rose Grondin <rose.grondin@gmail.com>
2020-10-13 18:01:52 -0400

    add state to burger builder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import React from "react";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 // importing styles
-import classes from './Burger.css'
+import classes from "./Burger.css";
 
 const Burger = (props) => {
+  const listIngredients = Object.keys(props.ingredients).map((igkey) => {
+    return [...Array(props.ingredients[igkey])].map((_, i) => {
+      return <BurgerIngredient key={igkey + i} type={igkey} />;
+    });
+  });
   return (
     <div className={classes.Burger}>
-        <BurgerIngredient type="bread-top"/>
-        <BurgerIngredient type="cheese"/>
-        <BurgerIngredient type="meat"/>
-        <BurgerIngredient type="bread-bottom"/>
+      <BurgerIngredient type="bread-top" />
+      {listIngredients}
+      <BurgerIngredient type="bread-bottom" />
     </div>
   );
 };
